2a16b4e6794afd6f5699a9001ee4ef8a2b2459e9
Yann Roirand <y.roirand@gmail.com>
2019-09-20 16:41:40 +0200

    TP2+_ALLWORKS_+botNaxLinks@custom

---
+++ ChoixResto/Migrations/201909191325256_Miguel-show.cs
@@ -0,0 +1,16 @@
+namespace ChoixResto.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Miguelshow : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
